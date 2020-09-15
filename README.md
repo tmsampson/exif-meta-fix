15a9bdd91e541866ae158c03024687d6da232e44
Thomas Sampson <tmsampson@gmail.com>
2020-09-15 08:25:33 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,5 @@
+Small utlity which strips all superfluous meta-data from .jpg files in current working directory.
+
+Original files are overwritten and basic properties such as "date taken" are retained.
+
+Fixes "Error 0x88982F52: There is too much metadata to be writen to the bitmap".
--- fix.bat
+++ fix.bat
@@ -1,6 +1,4 @@
 :: ====================================================================================================================================
-:: Strips all superfluous meta data from .jpg files in working directory, retaining date taken and other basic properties
-:: ====================================================================================================================================
 :: JHEAD DOWNLOAD: https://www.sentex.ca/~mwandel/jhead/jhead.exe
 ::    JHEAD USAGE: https://www.sentex.ca/~mwandel/jhead/usage.html
 :: ====================================================================================================================================
